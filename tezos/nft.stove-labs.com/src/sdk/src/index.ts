0f14f596ae774b0112d608d9bc482e8d86d65c22
Lena <lena.boruslav@gmail.com>
2019-09-11 18:25:52 +0300

    added tezos

---
+++ tezos/nft.stove-labs.com/src/sdk/src/index.ts
@@ -0,0 +1,12 @@
+// Setup the SDK & retrieve all existing tokens
+import { TezosNFT, Config } from "@stove-labs/tezos-nft-sdk";
+
+const config: Config = {
+    serverAddress: "http://alphanet-node.tzscan.io",
+    contractAddress: "KT1HCbJDVV3TzzuFuHCagwy8h6H78KEYERgL"
+};
+
+const nft = new TezosNFT(config);
+
+nft.getAllTokens()
+    .then(tokens => console.log("tokens", tokens));
